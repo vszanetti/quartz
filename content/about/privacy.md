77ec9b14106747d08086fea74c9a8eb85ccfe937
Vitor Zanetti <zanetti@air.local>
2024-10-06 19:27:47 -0300

    Quartz sync: Oct 6, 2024, 7:27 PM

---
+++ content/about/privacy.md
@@ -0,0 +1,6 @@
+---
+title: Privacy
+---
+<p style="text-align:justify;">I'm against the exhausting and invasive constant data grabs on websites. Yet, I run analytics on this website. Not the cookie-based kind, though — I use <a href="https://umami.is"><b>Umami</b></a>, a non-invasive and GDPR compliant analytics tool. There is no personal data being stored and certainly no data being sold.</p>
+
+<p style="text-align:justify;">It's just the bare minimum that helps me understand, through statistics, what is and what isn't working around here.</p>
--- quartz/components/Header.tsx
+++ quartz/components/Header.tsx
@@ -3,10 +3,20 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 const Header: QuartzComponent = ({ children }: QuartzComponentProps) => {
   return (
     <header>
+      <script
+        defer
+        src="http://129.148.30.45:3000/script.js"
+        data-website-id="95159691-bbc2-42e3-9147-4ebf634e05b7"
+      ></script>
       <nav>
         <ul>
           <li>
             <a href="/about">About</a>
+            <ul class="dropdown">
+              <li>
+                <a href="/about/privacy">Privacy</a>
+              </li>
+            </ul>
           </li>
           <li>
             <a href="/posts">Posts</a>
@@ -59,6 +69,56 @@ nav ul li a {
 nav ul li a:hover {
   color: #007BFF; /* Add hover effect for better UX */
 }
+
+/* Basic nav styles */
+nav ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+}
+
+nav ul li {
+  position: relative;
+  margin-right: 20px;
+}
+
+/* Hide the dropdown by default */
+nav ul li ul.dropdown {
+  display: none;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  list-style: none;
+  background-color: white;
+  padding: 0;
+  margin: 0;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
+  z-index: 10;
+}
+
+/* Show the dropdown on hover */
+nav ul li:hover ul.dropdown {
+  display: block;
+}
+
+/* Dropdown item styles */
+nav ul li ul.dropdown li {
+  margin: 0;
+}
+
+nav ul li ul.dropdown li a {
+  padding: 6px 8px;
+  display: block;
+  text-decoration: none;
+  color: black;
+  white-space: nowrap;
+}
+
+nav ul li ul.dropdown li a:hover {
+  background-color: #B22222;
+}
+
 `
 
 export default (() => Header) satisfies QuartzComponentConstructor
