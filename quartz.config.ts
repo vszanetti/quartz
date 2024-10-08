7d0110627d7d1c4695e7c52c46a6da96e890150f
Vitor Zanetti <zanetti@cliente-150-162-179-98.wireless.ufsc.br>
2024-10-08 10:59:37 -0300

    Quartz sync: Oct 8, 2024, 10:59 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "serrazanetti.xyz",
+    baseUrl: "https://serrazanetti.xyz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
