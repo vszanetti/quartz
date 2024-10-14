75e214225cbd421d3c575f10deab3af4ca9d231c
Vitor Zanetti <zanetti@air.local>
2024-10-14 17:16:35 -0300

    Quartz sync: Oct 14, 2024, 5:16 PM

--- quartz/components/Header.tsx
+++ quartz/components/Header.tsx
@@ -5,7 +5,7 @@ const Header: QuartzComponent = ({ children }: QuartzComponentProps) => {
     <header>
       <script
         defer
-        src="http://129.148.30.45:3000/script.js"
+        src="https://serrazanetti.xyz/script.js" // Use absolute URL
         data-website-id="95159691-bbc2-42e3-9147-4ebf634e05b7"
       ></script>
       <nav>
@@ -28,94 +28,5 @@ const Header: QuartzComponent = ({ children }: QuartzComponentProps) => {
     </header>
   )
 }
-
-// CSS styling to resemble Morrick.me's layout
-Header.css = `
-header {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between; /* Ensures space between the title and the menu */
-  margin: 2rem 0;
-  gap: 1.5rem;
-}
-
-header h1 {
-  margin: 0;
-  flex: auto;
-}
-
-nav ul {
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-  display: flex;
-  gap: 2rem; /* Space between menu items */
-}
-
-nav ul li {
-  display: inline;
-}
-
-nav ul li a {
-  text-decoration: none;
-  font-size: 1rem;
-  color: #B22222; /* You can customize the color */
-  transition: color 0.3s ease;
-}
-
-nav ul li a:hover {
-  color: #007BFF; /* Add hover effect for better UX */
-}
-
-/* Basic nav styles */
-nav ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  display: flex;
-}
-
-nav ul li {
-  position: relative;
-  margin-right: 20px;
-}
-
-/* Hide the dropdown by default */
-nav ul li ul.dropdown {
-  display: none;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  list-style: none;
-  background-color: white;
-  padding: 0;
-  margin: 0;
-  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2);
-  z-index: 10;
-}
-
-/* Show the dropdown on hover */
-nav ul li:hover ul.dropdown {
-  display: block;
-}
-
-/* Dropdown item styles */
-nav ul li ul.dropdown li {
-  margin: 0;
-}
-
-nav ul li ul.dropdown li a {
-  padding: 6px 8px;
-  display: block;
-  text-decoration: none;
-  color: black;
-  white-space: nowrap;
-}
-
-nav ul li ul.dropdown li a:hover {
-  background-color: #B22222;
-}
-`
 
 export default (() => Header) satisfies QuartzComponentConstructor
